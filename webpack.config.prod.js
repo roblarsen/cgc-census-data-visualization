cb40dfa74bfdf5a9fb2489688578135b15c86e9c
Rob Larsen <rob@drunkenfist.com>
2025-04-22 17:07:34 +0000

    prod

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -13,7 +13,6 @@ module.exports = merge(common, {
       patterns: [
         { from: "img", to: "img" },
         { from: "css", to: "css" },
-        { from: "scripts/vendor", to: "scripts/vendor" },
         { from: "icon.svg", to: "icon.svg" },
         { from: "favicon.ico", to: "favicon.ico" },
         { from: "robots.txt", to: "robots.txt" },
