575fddbe8c433a40b032c594269fc55386c0ae64
Rob Larsen <rob@drunkenfist.com>
2025-04-27 17:47:00 +0000

    Completed table

--- scripts/drawTable.ts
+++ scripts/drawTable.ts
@@ -4,31 +4,20 @@ import { Comic } from "./Comic";
 
 export function drawTable(data: Comic[]) {
  
-  d3
-    .select("#table")
-    .append("table")
-    .append("thead")
-    .append("tr")
-    .html(() => {
-      const th =
-        `
-        <th>Title</th>
-        <th>Value (OSPG)</th>
-        <th>Date</th>
-        `
-      return th;
-    });
 
-  d3.select("#table table")
+
+  d3.select("#datagrid")
+    .selectAll("tr")
     .data(data)
     .enter()
     .append("tr")
     .html((d:Comic)=> {
      const td =
      `
-    <td>${d.title}</td>
-    <td> ${d.value}</td>
-    <td>${d.date}</td>
+      <td>${d.rank}</td>      
+      <td>${d.title}</td>
+      <td> ${d3.format("$,")(d.value)}</td>
+      <td>${d.date}</td>
     `
       return td;
     }) 
